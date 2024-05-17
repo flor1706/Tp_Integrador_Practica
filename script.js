51793d3d3c32ba89cfe4a37b42c81c1b1536ae58
martinsintierra <gomezdeliamartin@gmail.com>
2024-05-17 01:16:20 -0300

    Reemplazo de "var" por "let" en codigo Javascript.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Validación del formulario de contacto
 document.getElementById('contactForm').addEventListener('submit', function(event) {
-    var nombre = document.getElementById('nombre').value;
-    var email = document.getElementById('email').value;
-    var consulta = document.getElementById('consulta').value;
-    var motivo = document.getElementById('motivo').value;
+    let nombre = document.getElementById('nombre').value;
+    let email = document.getElementById('email').value;
+    let consulta = document.getElementById('consulta').value;
+    let motivo = document.getElementById('motivo').value;
 
     if (nombre === '' || email === '' || consulta === '' || motivo === '') {
         event.preventDefault(); // Evitar que el formulario se envíe
