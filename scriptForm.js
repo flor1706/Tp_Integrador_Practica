231cde8d978063a59b818bf97994de5b2fee331c
Maca Santana <maky.sants@gmail.com>
2024-05-22 00:30:30 -0300

    Listar Comentarios de formulario en Home

--- scriptForm.js
+++ scriptForm.js
@@ -1,31 +1,18 @@
+var listaTestimonios = JSON.parse(sessionStorage.getItem("item"));
 
-    console.log(sessionStorage.getItem("item"));
-    var listaTestimonios = JSON.parse(sessionStorage.getItem("item"))
-    console.log(listaTestimonios)
+mostrarItems(listaTestimonios);
 
+function mostrarItems(listaTestimonios) {
+    var contenidoItems = document.getElementsByClassName('testimonios');
 
-    function mostrarItems(listaTestimonios) {
-
-        var contenidoItems = document.getElementsByClassName('testimonios');
-        contenidoItems.innerHTML = '' ;
-
-        listaTestimonios.forEach((listaT)=>{
+    if (contenidoItems) {
+       
+        listaTestimonios.forEach((listaT) => {
             var etiquetaItem = document.createElement('p');
-            etiquetaItem.textContent = `${listaT.consulta}`;
-            contenidoItems.appendChild(etiquetaItem)
-
-        })
-        
+            etiquetaItem.textContent = `"${listaT.consulta}" - ${listaT.nombre}`;
+            contenidoItems.appendChild(etiquetaItem);
+        });
+    } else {
+        console.log('No se encontró ningún elemento con la clase "testimonios".');
     }
-
-    // function displayData() {
-    //     var dataList = JSON.parse(localStorage.getItem('dataList')) || [];
-    //     var listContainer = document.getElementById('dataList');
-    //     listContainer.innerHTML = '';
-        
-    //     dataList.forEach(function(data) {
-    //       var listItem = document.createElement('li');
-    //       listItem.textContent = `Nombre: ${data.name}, Email: ${data.email}`;
-    //       listContainer.appendChild(listItem);
-    //     });
-    //   }
+}
